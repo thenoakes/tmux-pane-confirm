a38fc965cd5fd30b9864f4ee9d28c5d1d92d2a58
Matt Noakes <matthew.noakes@sparx.co.uk>
2025-11-15 18:20:36 +0000

    Offset menu below status

--- scripts/popup-confirm.sh
+++ scripts/popup-confirm.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 tmux display-menu \
   -T "#[align=centre]Close this pane?" \
   -x P \
-  -y 1 \
+  -y 2 \
   "" "" "" \
   "y" "Yes (Enter / Click)" "kill-pane" \
   "Enter" "Yes (Enter / Click)" "kill-pane" \
